2b736192100632a290315782fb4c5f38ae63e83f
hanyuxie1234 <hanyuxie@brandeis.edu>
2023-03-26 13:27:29 -0400

    run pylint for transaction.py

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -1,11 +1,28 @@
+"""
+import SQL
+"""
 import sqlite3
-##############this should have no problem############
+
+
+#this has pylint 9.7/10
 class Transaction:
+    """
+    class transaction
+    """
+
     def __init__(self, filename):
+        """
+        intiate the file
+        :param filename:
+        """
         self.conn = sqlite3.connect(filename)
         self.create_table()
 
     def create_table(self):
+        """
+        create the table
+        :return:
+        """
         cursor = self.conn.cursor()
         cursor.execute("""CREATE TABLE IF NOT EXISTS transactions (
                         id INTEGER PRIMARY KEY,
@@ -17,17 +34,42 @@ class Transaction:
         self.conn.commit()
 
     def add_transaction(self, amount, category, date, description):
-        c = self.conn.cursor()
-        c.execute("INSERT INTO transactions (amount, category, date, description) VALUES (?, ?, ?, ?)", (amount, category, date, description))
+        """
+        add transaction
+        :param amount:
+        :param category:
+        :param date:
+        :param description:
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "INSERT INTO transactions (amount, category, date, description) VALUES (?, ?, ?, ?)", (
+                amount, category, date, description))
         self.conn.commit()
 
     def delete_transaction(self, transaction_id):
-        c = self.conn.cursor()
-        c.execute("DELETE FROM transactions WHERE id=?", (transaction_id,))
+        """
+        delete transaction
+        :param transaction_id:
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute("DELETE FROM transactions WHERE id=?", (transaction_id,))
         self.conn.commit()
 
-    def update_transaction(self, transaction_id, amount=None, category=None, date=None, description=None):
-        c = self.conn.cursor()
+    def update_transaction(self, transaction_id,
+                           amount=None, category=None, date=None, description=None):
+        """
+        update transaction
+        :param transaction_id:
+        :param amount:
+        :param category:
+        :param date:
+        :param description:
+        :return:
+        """
+        cursor = self.conn.cursor()
         updates = {}
         if amount:
             updates['amount'] = amount
@@ -37,49 +79,90 @@ class Transaction:
             updates['date'] = date
         if description:
             updates['description'] = description
-        query = "UPDATE transactions SET " + ", ".join(f"{key}=?" for key in updates.keys()) + " WHERE id=?"
+        query = "UPDATE transactions SET " + ", ".join(
+            f"{key}=?" for key in updates.keys()) + " WHERE id=?"
         params = tuple(updates.values()) + (transaction_id,)
-        c.execute(query, params)
+        cursor.execute(query, params)
         self.conn.commit()
 
     def get_transactions(self):
-        c = self.conn.cursor()
-        c.execute("SELECT * FROM transactions")
-        return c.fetchall()
+        """
+        get transaction
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM transactions")
+        return cursor.fetchall()
 
     def get_categories(self):
-        c = self.conn.cursor()
-        c.execute("SELECT DISTINCT category FROM transactions")
-        return [row[0] for row in c.fetchall()]
+        """
+        get by category
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT DISTINCT category FROM transactions")
+        return [row[0] for row in cursor.fetchall()]
 
     def summarize_by_date(self):
-        c = self.conn.cursor()
-        c.execute("SELECT date, SUM(amount) FROM transactions GROUP BY date")
-        return c.fetchall()
+        """
+        summarize by date
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "SELECT date, SUM(amount) FROM transactions GROUP BY date")
+        return cursor.fetchall()
 
     def summarize_by_month(self):
-        c = self.conn.cursor()
-        c.execute("SELECT strftime('%Y-%m', date) AS month, SUM(amount) FROM transactions GROUP BY month")
-        return c.fetchall()
+        """
+        summarize by month
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "SELECT strftime('%Y-%m', date) AS month, SUM(amount) FROM transactions GROUP BY month")
+        return cursor.fetchall()
 
     def summarize_by_year(self):
-        c = self.conn.cursor()
-        c.execute("SELECT strftime('%Y', date) AS year, SUM(amount) FROM transactions GROUP BY year")
-        return c.fetchall()
+        """
+        summarize by year
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "SELECT strftime('%Y', date) AS year, SUM(amount) FROM transactions GROUP BY year")
+        return cursor.fetchall()
 
     def summarize_by_category(self):
-        c = self.conn.cursor()
-        c.execute("SELECT category, SUM(amount) FROM transactions GROUP BY category")
-        return c.fetchall()
+        """
+        summarize by category
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "SELECT category, SUM(amount) FROM transactions GROUP BY category")
+        return cursor.fetchall()
 
     def add_category(self, category):
-        c = self.conn.cursor()
-        c.execute("INSERT INTO transactions (category) VALUES (?)", (category,))
+        """
+        add category
+        :param category:
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "INSERT INTO transactions (category) VALUES (?)", (
+                category,))
         self.conn.commit()
 
     def modify_category(self, old_category, new_category):
-        c = self.conn.cursor()
-        c.execute("UPDATE transactions SET category=? WHERE category=?", (new_category, old_category))
+        """
+        modify category
+        :param old_category:
+        :param new_category:
+        :return:
+        """
+        cursor = self.conn.cursor()
+        cursor.execute("UPDATE transactions SET category=? WHERE category=?", (
+            new_category, old_category))
         self.conn.commit()
-
-        
