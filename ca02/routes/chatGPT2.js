31d98e334c2aacafa15c465e6b2fe2152d6ca04f
Clark Xu <yilinxu@brandeis.edu>
2023-05-01 10:29:18 -0400

    update

--- ca02/routes/chatGPT2.js
+++ ca02/routes/chatGPT2.js
@@ -2,10 +2,11 @@ const express = require('express');
 const axios = require('axios');
 const GPTModel = require('../models/GPT');
 const router = express.Router();
-const baseURL = "you key";
-const apiKey = "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";
+const baseURL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+const apiKey = "you key"
 
-const chatGPT = async (inputText, userId) => {
+
+/*const chatGPT = async (inputText, userId) => {
     
     const instance = axios.create({
         headers: {
@@ -41,7 +42,45 @@ const chatGPT = async (inputText, userId) => {
     await newEntry.save();
 
     return output;
-};
+};*/
+
+
+const generateResponse = async (prompt) => {
+    const apiKey = "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"; // Replace with your valid API key
+    const baseURL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+  
+    // Set up the payload for the GPT-3 API
+    const payload = {
+      prompt: prompt,
+      max_tokens: 150,
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    };
+  
+    try {
+      const response = await fetch(baseURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify(payload),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      const output = data.choices[0].text;
+      return output;
+    } catch (error) {
+      console.error('Error calling GPT-3 API:', error);
+      return 'Sorry, an error occurred while processing your request.';
+    }
+  };
 
 router.get('/chat2', (req, res)=> {
     res.render("chat2")
@@ -51,7 +90,7 @@ router.post('/chat2', async (req, res) => {
     console.log(req.body.inputText)
     const inputText = req.body.inputText;
     const userId = req.user._id;
-    const response = await chatGPT(inputText, userId);
+    const response = await generateResponse(inputText, userId);
     res.send(response);
 });
 
